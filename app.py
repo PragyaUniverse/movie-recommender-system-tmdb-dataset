ce5f3bbb14fe8c3452d34055a70027e98f73470e
PragyaUniverse <pragyasumbly5296@gmail.com>
2026-04-27 00:35:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,60 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
-import streamlit as st
 import requests
 
-def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+app = Flask(__name__)
+CORS(app)
+
+# Load your saved files
+movies = pickle.load(open('movie_list.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+# 🔑 TMDB API KEY (get from https://www.themoviedb.org/)
+API_KEY = "YOUR_API_KEY_HERE"
+
+def fetch_poster(movie_title):
+    url = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={movie_title}"
+    data = requests.get(url).json()
+
+    if data['results']:
+        poster_path = data['results'][0].get('poster_path')
+        if poster_path:
+            return "https://image.tmdb.org/t/p/w500" + poster_path
+    
+    return "https://via.placeholder.com/300x450?text=No+Image"
+
 
 def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
+    try:
+        index = movies[movies['title'] == movie].index[0]
+    except:
+        return []
+
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
-    recommended_movie_posters = []
+
+    recommendations = []
     for i in distances[1:6]:
-        # fetch the movie poster
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
+        title = movies.iloc[i[0]].title
+        recommendations.append({
+            "title": title,
+            "poster": fetch_poster(title)
+        })
 
-    return recommended_movie_names,recommended_movie_posters
-
-
-st.header('Movie Recommender System')
-movies = pickle.load(open('model/movie_list.pkl','rb'))
-similarity = pickle.load(open('model/similarity.pkl','rb'))
-
-movie_list = movies['title'].values
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
-)
-
-if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.beta_columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
-
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+    return recommendations
 
 
+@app.route('/recommend', methods=['POST'])
+def recommend_api():
+    data = request.get_json()
+    movie = data['movie']
+
+    results = recommend(movie)
+
+    return jsonify({
+        "recommendations": results
+    })
 
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
